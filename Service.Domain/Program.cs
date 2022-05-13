872cdf180a0f2f65c7f99b22513dfe16bb3cb7a0
thiago matttos <thiagoapoloniooo@hotmail.com>
2022-05-13 17:15:43 -0300

    Adicionar arquivos de projeto.

---
+++ Service.Domain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Service.Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Servico.Configuracao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Servico.Configuracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
